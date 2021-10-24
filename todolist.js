6b981bf692001bcf99e4f8b9d4043192def8d58a
Ignacio <nachonar2020@gmail.com>
2021-10-24 12:04:47 -0300

    commit

---
+++ todolist.js
@@ -0,0 +1,49 @@
+const form = document.querySelector("form");
+const input = document.querySelector("input")
+input.setAttribute("placeholder", "Write your items!")
+const container = document.querySelector("#container")
+
+// TESTING remove()
+const remove = container.querySelector("div")
+remove.remove()
+
+const ul = document.createElement("ul")
+ul.style.padding = "0px"
+container.appendChild(ul)
+
+function handlerSubmit(e) {
+    e.preventDefault(); 
+
+// NO EMPTY VALUE
+const empty = (str) => !str.trim().length;
+    if (empty(input.value)) {
+        return
+    }
+
+// ITEM PROPERTIES
+    const li = document.createElement("li")
+    li.classList.add("task-container")
+    li.innerText = input.value
+    ul.appendChild(li)
+    const del = document.createElement("button") 
+    del.classList.add("btn")
+    del.innerText = "❌"
+    li.append(del)
+
+//  DELETE ITEM
+    function deleteDiv (e) {
+       del.parentNode.remove(); 
+    }
+    del.addEventListener("click", deleteDiv)
+
+
+// RESET INPUT TEXT
+    if (e = "submit") {
+    input.value=""
+    }
+}
+   
+form.addEventListener("submit", handlerSubmit)
+
+
+
